34a74b7a9ca4cff87d2c11b888dae18082048c49
Thiago Kienbaum <thiago.kienbaum@ibratan.com.br>
2020-06-18 18:51:05 -0300

    Refactoring

--- extension.js
+++ extension.js
@@ -1,21 +1,51 @@
+const start = () => {
+    const interval = setInterval(() => {
+    const navbar = document.querySelector('._1QUKR');                
+        if(navbar != null) {            
+            clearInterval(interval);            
+            audioSpeedController(navbar)
+        }        
+    }, 1000);    
+}
+
+const audioSpeedController = (navbar) => {   
+    const playbackRateElement = document.createElement('p');
+    playbackRateElement.classList.add('playbackRateElement');
+    const decreaseAudioRate = document.createElement('button');
+    playbackRateElement.innerHTML = '1.00x'
+    decreaseAudioRate.innerHTML = '- ';
+    decreaseAudioRate.classList.add('decreaseAudioRate');    
+    const increaseAudioRate = document.createElement('button');
+    increaseAudioRate.innerHTML = ' +'; 
+    increaseAudioRate.classList.add('increaseAudioRate');
+
+    decreaseAudioRate.addEventListener('click', () => {
+        const audios = document.querySelectorAll('audio');   
+        audios.forEach(audio => {
+            if(audio.playbackRate >= 0.5) {
+                audio.playbackRate -= 0.25
+                playbackRateElement.innerHTML = `${audio.playbackRate.toFixed(2)}x` 
+            }        
+        });            
+    });
+
+    increaseAudioRate.addEventListener('click', () => {
+        const audios = document.querySelectorAll('audio');   
+        audios.forEach(audio => {
+            if(audio.playbackRate <= 1.75) {
+                audio.playbackRate += 0.25 
+                playbackRateElement.innerHTML = `${audio.playbackRate.toFixed(2)}x` 
+            }        
+        });            
+    });       
+    
+    renderExtension(navbar, playbackRateElement, decreaseAudioRate, increaseAudioRate);
+};
+
+const renderExtension = (navbar, playbackRateElement, decreaseAudioRate, increaseAudioRate) => {
+    navbar.appendChild(decreaseAudioRate);
+    navbar.appendChild(playbackRateElement);
+    navbar.appendChild(increaseAudioRate); 
+}
+
 start();
-
-const start = async () => {
-    const menuHeader = await getMenuHeader();
-    audioSpeedController(menuHeader)            
-}
-
-const getMenuHeader = () => {
-    document.querySelector('._1QUKR');
-}
-
-const audioSpeedController = (menuHeader) => {
-    const button = document.createElement('button')
-        button.innerHTML = '2x'
-        button.classList.add("twoTimesButton")
-        button.addEventListener('click', () => {
-            const audios = document.querySelectorAll('audio');
-            audios.forEach(audio => audio.playbackRate = 2)
-        })
-        menuHeader.appendChild(button)
-}
